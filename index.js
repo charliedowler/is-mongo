8c3c87417c1442c58c51c2c978503930c2c308ce
charliedowler <charliedowler15@gmail.com>
2014-08-22 18:59:04 +0100

    Setup promises

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 'use strict';
 var exec = require('child_process').exec;
+var Q = require('q');
 module.exports = function (callback) {
+  var deferred = Q.defer();
   exec('mongo --eval "printjson(db.serverStatus())"', function(err, stdout, stderr) {
-    callback((err) ? false : true);
+    var result = (err) ? false : true;
+    if (callback) callback(result);
+    deferred.resolve(result);
   });
+  return deferred.promise;
 };
--- test/test.js
+++ test/test.js
@@ -2,9 +2,15 @@
 var assert = require('assert');
 var isMongo = require('../index');
 
-it('Should recognise mongoDB', function(done) {
-  isMongo(function(running) {
-    assert.equal(running, true, 'MongoDB is running');
+it('Should recognise mongoDB is running - promise', function(done) {
+  isMongo().then(function(running) {
+    assert.equal(running, true);
     done();
-  })
+  });
+});
+it('Should recognise mongoDB is running - callback', function(done) {
+  isMongo(function(running) {
+    assert.equal(running, true);
+    done();
+  });
 });
